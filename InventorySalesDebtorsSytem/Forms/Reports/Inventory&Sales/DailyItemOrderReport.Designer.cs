98682eceb0d91d2a2ac473f5b51197bbb0ef0acd
tthotamune <tthotamune@yahoo.com>
2013-10-26 02:52:48 +0530

    Add Changes

--- InventorySalesDebtorsSytem/Forms/Reports/Inventory&Sales/DailyItemOrderReport.Designer.cs
+++ InventorySalesDebtorsSytem/Forms/Reports/Inventory&Sales/DailyItemOrderReport.Designer.cs
@@ -30,30 +30,41 @@
         {
             this.dateTimePickerDate = new System.Windows.Forms.DateTimePicker();
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // dateTimePickerDate
             // 
-            this.dateTimePickerDate.Location = new System.Drawing.Point(196, 41);
+            this.dateTimePickerDate.Location = new System.Drawing.Point(131, 48);
             this.dateTimePickerDate.Name = "dateTimePickerDate";
             this.dateTimePickerDate.Size = new System.Drawing.Size(200, 20);
             this.dateTimePickerDate.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(154, 86);
+            this.button1.Location = new System.Drawing.Point(114, 100);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(75, 27);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "Print";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(66, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Date";
             // 
             // DailyItemOrderReport
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(408, 155);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dateTimePickerDate);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
@@ -67,6 +78,7 @@
             this.Text = "Daily Item Order Report";
             this.Load += new System.EventHandler(this.DailyItemOrderReport_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -74,5 +86,6 @@
 
         private System.Windows.Forms.DateTimePicker dateTimePickerDate;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
--- InventorySalesDebtorsSytem/Forms/Reports/Inventory&Sales/DailyItemOrderReport.cs
+++ InventorySalesDebtorsSytem/Forms/Reports/Inventory&Sales/DailyItemOrderReport.cs
@@ -27,17 +27,105 @@ namespace InventorySalesDebtorsSytem.Forms.Reports.Inventory_Sales
 
         private void button1_Click(object sender, EventArgs e)
         {
-            DateTime currentDate = DateTime.Now;
+            DateTime currentDate = DateTime.Now.Date;
             DateTime reportDate = dateTimePickerDate.Value;
 
-            var salesOrder = (from s in db.SalesOrderDets
-                             where s.SalesOrderHed.TxnDate == reportDate
+            // Sales orders
+            var salesOrderOnDate = (from s in db.SalesOrderDets
+                             where s.SalesOrderHed.DelieveryDate == reportDate
                              select new { 
                                 s.ItemCode,
                                 s.Item.ItemName,
-                                s.Quantity
+                                s.Quantity,
+                                s.SalesOrderHed.DelieveryDate
                              }).ToList();
-            
+
+            var salesOrders = (from s in db.SalesOrderDets
+                               where ((s.SalesOrderHed.DelieveryDate < reportDate) &&  (currentDate <= s.SalesOrderHed.DelieveryDate))
+                              select new
+                              {
+                                  s.ItemCode,
+                                  s.Item.ItemName,
+                                  s.Quantity,
+                                  s.SalesOrderHed.DelieveryDate
+                              }).ToList();
+
+
+            // Standard Orders
+            var standardOrdOnDate = (from s in db.SPSDets
+                                     where s.Date == reportDate
+                                     select new
+                                     {
+                                         s.SPSHed.ItemCode,
+                                         s.SPSHed.Item.ItemName,
+                                         Quantity=s.Qty,
+                                         DelieveryDate=s.Date
+                                     }).ToList();
+
+            var standardOrds = (from s in db.SPSDets
+                                     where s.Date < reportDate && s.Date>=currentDate
+                                     select new
+                                     {
+                                         s.SPSHed.ItemCode,
+                                         s.SPSHed.Item.ItemName,
+                                         Quantity=s.Qty,
+                                         DelieveryDate=s.Date
+                                     }).ToList();
+
+            var OnDateOrders = salesOrderOnDate.ToList();
+            OnDateOrders.AddRange(standardOrdOnDate);
+
+            var orders = salesOrders.ToList();
+            orders.AddRange(standardOrds);
+
+            var finalOnDateVal = from s in OnDateOrders
+                           group s by new { s.ItemCode, s.ItemName }
+                               into g
+                                     select new { ItemCode = g.Key.ItemCode, ItemName=g.Key.ItemName, Item_Type = "Finish Goods", QOH = 0, Quantity = g.Sum(s => s.Quantity) };
+
+            //var finalVal = from s in orders
+            //               group s by new { s.ItemCode, s.ItemName }
+            //                   into g
+            //                   select new { g.Key.ItemCode, g.Key.ItemName, Quantity = g.Sum(s => s.Quantity) };
+
+           
+            var stores=db.Companies.FirstOrDefault().StoresLoc;
+            var branch=db.Companies.FirstOrDefault().CurrentBranchCode;
+
+            var cursor=string .Empty;
+            foreach (var item in finalOnDateVal)
+            {
+      
+                
+                //var boqs = (from d in db.BoqDets join i in db.ItemQOHs 
+                //                                on d.ItemCode equals i.ItemCode
+                //           where d.BoqHed.BoqCode == item.ItemCode && i.LocationCode==stores && i.BranchCode==branch
+                //           select new { d.ItemCode, d.Item.ItemName, Item_Type = "Raw ", QOH = i.QOH, Quantity = ((d.Quantity / d.BoqHed.UsableQty) * item.Quantity) }).ToList();
+
+                var boqs = (from d in db.BoqDets
+                            join i in db.ItemQOHs
+                                  on d.ItemCode equals i.ItemCode
+                            where d.BoqHed.BoqCode == item.ItemCode && i.LocationCode == stores && i.BranchCode == branch
+                            select new {finishItem=item.ItemCode, d.ItemCode, d.Item.ItemName, QOH = i.QOH, Quantity = ((d.Quantity / d.BoqHed.UsableQty) * item.Quantity) }).ToList();
+
+                //cursor += boqs.ToList();
+              //  cursor.AddRange(boqs);
+            }
+
+            DataTable dt = Helpers.LINQToDataTable(cursor);
+
+            if (dt.Rows.Count <= 0)
+            {
+                MessageBox.Show("No Records Found", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            dt.TableName = "FinisheGoods";
+            ReportViewer r = new ReportViewer();
+
+            string Date = dateTimePickerDate.Value.ToShortDateString();
+
+
+            r.PrintReport(System.IO.Directory.GetCurrentDirectory() + @"\Reports\FinisheGoods", dt, db, this.Text + "" + " as at " + Date);
 
 
         }
